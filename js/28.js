86a6a8ba2604af1c8e2e5d47b35a69d580470892
Raquel Alvarado <zilpa.alvarado@unah.hn>
2024-12-21 10:26:59 -0600

    selectores en js

---
+++ js/28.js
@@ -0,0 +1,11 @@
+//SELECTORES
+
+const heading = document.querySelector(".heading");
+
+console.log(heading);
+console.log(heading.textContent);
+console.log(heading.classList);
+console.log(heading.tagName);
+
+const enlaces = document.querySelectorAll(".navegacion a");
+console.log(enlaces);
