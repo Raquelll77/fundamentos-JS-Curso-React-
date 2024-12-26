c693bf49341bb14050fa080f54735dd58fb2bc56
Raquel Alvarado <zilpa.alvarado@unah.hn>
2024-12-26 16:52:40 -0600

    validando formularios y mostrando alertas

--- js/32.js
+++ js/32.js
@@ -7,10 +7,37 @@ formulario.addEventListener("submit", (e) => {
   const nombre = document.querySelector("#nombre").value;
   const password = document.querySelector("#password").value;
 
+  // prevenir nuevas alertas
+
+  const alertaPrevia = document.querySelector(".alerta");
+
+  alertaPrevia?.remove();
+
+  const alerta = document.createElement("DIV");
+
+  alerta.classList.add(
+    "alerta",
+    "text-white",
+    "uppercase",
+    "text-sm",
+    "text-center",
+    "p-2",
+    "font-black"
+  );
+
   if (nombre === "" || password === "") {
-    console.log("todos los campos son obligatorios");
+    alerta.textContent = "Todos los campos son obligatorios";
+    alerta.classList.add("bg-red-500");
   } else {
-    console.log("Enviando el formulario TODO OK......");
-    console.log(`nombre: ${nombre}, password: ${password}`);
+    alerta.textContent = `nombre: ${nombre}, password: ${password}`;
+    alerta.classList.add("bg-green-500");
   }
+
+  console.log(alerta);
+
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.remove();
+  }, 3000);
 });
