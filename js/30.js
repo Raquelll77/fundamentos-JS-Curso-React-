4dda588634819f9437bb51b3b52b4aea33e1bc8f
Raquel Alvarado <zilpa.alvarado@unah.hn>
2024-12-26 11:08:29 -0600

    eventos del DOM clicks

---
+++ js/30.js
@@ -0,0 +1,19 @@
+//EVENTOS DEL DOM - CLICKS
+
+const heading = document.querySelector(".heading");
+const enlaces = document.querySelectorAll(".navegacion a");
+
+heading.addEventListener("mouseenter", () => {
+  heading.textContent = "Nuevo heading al dar click";
+});
+
+heading.addEventListener("mouseleave", () => {
+  heading.textContent = "Saliendo...";
+});
+
+enlaces.forEach((enlace) => {
+  enlace.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.target.textContent = "diste click";
+  });
+});
